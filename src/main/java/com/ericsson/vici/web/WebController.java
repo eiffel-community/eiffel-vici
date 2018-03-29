11fdb532fda7bbf44c61c7a1e729501a64f49672
Jonathan Wahlund <jonathan.wahlund@adtraction.com>
2018-03-29 23:05:22 +0200

    Angular init

--- src/main/java/com/ericsson/vici/web/WebController.java
+++ src/main/java/com/ericsson/vici/web/WebController.java
@@ -17,13 +17,31 @@
 package com.ericsson.vici.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class WebController {
-    @RequestMapping("/")
-    public String greeting() {
-        return "index";
-    }
+//    @RequestMapping("/")
+//    public String greeting() {
+//        return "index";
+//    }
 
+	@GetMapping("/resource")
+	@ResponseBody
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+
+	@GetMapping(value = "/{path:[^.]*}")
+	public String redirect() {
+		return "forward:/";
+	}
 }
