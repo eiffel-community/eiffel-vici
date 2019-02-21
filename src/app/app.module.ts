5d128790135459766f0780422e2756ab3ec45932
Jonathan Wahlund <jonathan@jonathanwahlund.se>
2019-02-21 12:35:09 +0100

    UI-Bugfix for details datatables-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,7 +41,7 @@ const appRoutes: Routes = [
         HttpClientModule,
         FormsModule,
         MomentModule,
-        NgbModule.forRoot(),
+        NgbModule,
         AngularFontAwesomeModule,
         DataTablesModule
     ],
